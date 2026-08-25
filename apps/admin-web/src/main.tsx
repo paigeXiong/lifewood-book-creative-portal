14efa6ccadb03cdb1cfce50aa2843ae1fed5fc2d
paigeXiong <paige.xiong@lifewood.com>
2026-08-25 12:15:37 +0800

    style: remove decorative borders from admin surfaces

--- apps/admin-web/src/main.tsx
+++ apps/admin-web/src/main.tsx
@@ -7,6 +7,7 @@ import "@lifewood/i18n";
 import "./i18n";
 import { App } from "./App";
 import "./styles.css";
+import "./surface-theme.css";
 
 const queryClient = new QueryClient({ defaultOptions: { queries: { retry: false, staleTime: 10_000, refetchOnWindowFocus: false } } });
 
